e22d6f8587a5e21d6c94adf838c0aedf30e4be66
Dakshbir <144359831+Dakshbir@users.noreply.github.com>
2023-10-02 14:50:34 +0530

    Add files via upload

---
+++ codechef_dakshbir.py
@@ -0,0 +1,30 @@
+def fibonacci_(n):
+    if n<= 0:
+        print("Incorrect input")
+    
+    elif n == 1:
+        return 0
+    
+    elif n == 2:
+        return 1
+    
+    else:
+        return fibonacci_(n-1)+fibonacci_(n-2)
+ 
+def golden_ratio(n):
+    if n==0:
+        print("Incorrect input")
+    elif n==1:
+        print("golden ratio cannot be calculated")
+    elif n==2:
+        print("division by 0 not possible")
+    
+    else:
+        n1=fibonacci_(n)
+        n2=fibonacci_(n-1)
+        return n1/n2
+
+
+n=int(input("Enter a number to calculate its fibonacci sequence and golden ratio"))
+print("Fibonacci Sequence number is:",fibonacci_(n))
+print("Golden ratio is:",golden_ratio(n))
